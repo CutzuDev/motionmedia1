e241e3aa230ef55cdb54b271777688e81bf56a00
CutzuDev <alexfarkas05@gmail.com>
2023-05-23 18:45:48 +0300

    Updates

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -22,7 +22,6 @@ const ServicesList: ServicesListItem[] = [
     features: [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
       "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     ],
     icon: (
       <svg
