852ac4d543dad3debcb7ca9419b963f63b880272
CutzuDev <alexfarkas05@gmail.com>
2023-05-24 19:22:29 +0300

    Updates :)

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -2,55 +2,58 @@ function Contact() {
   return (
     <section
       id="contact"
-      className="flex w-full select-none flex-col items-center justify-center gap-4 p-5 pb-10"
+      className="my-10 flex w-full select-none flex-col items-center justify-center pb-[120px] p-5"
     >
-      <div className="flex flex-col items-center justify-between border border-gray-300 border-opacity-30  p-10 py-20 h-96 backdrop-blur-md rounded-lg">
-        <a
-          href="https://calendly.com/motionmediaagency/30min"
-          target="_blank"
-          className="flex items-center justify-center gap-4"
-        >
-          <span className="text-4xl font-black sm:text-5xl">Via callendly</span>
-          <div className="rounded-full border border-gray-300 border-opacity-30 bg-white bg-opacity-5 p-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="h-8 w-8"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-              />
-            </svg>
-          </div>
-        </a>
-        <span className="text-2xl">or</span>
-        <a
-          href="mailto:motionmediaagencysrl@gmail.com"
-          className=" flex items-center justify-center gap-4"
-        >
-          <span className="text-4xl font-black sm:text-5xl">Via email</span>
-          <div className="rounded-full border border-gray-300 border-opacity-30 bg-white bg-opacity-5 p-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="h-8 w-8"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-              />
-            </svg>
-          </div>
-        </a>
+      <div className="flex flex-col items-center justify-between gap-[60px] rounded-lg w-full sm:w-auto  border border-gray-300 border-opacity-30 px-5 py-5 sm:p-10 backdrop-blur-md">
+        <span className="text-5xl font-black">Contact</span>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <a
+            href="https://calendly.com/motionmediaagency/30min"
+            target="_blank"
+            className="flex w-full items-center justify-center gap-4 rounded-lg border border-gray-300 border-opacity-30 bg-white bg-opacity-5 px-8 py-4"
+          >
+            <span className="text-3xl font-black ">Callendly</span>
+            <div className="rounded-full ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="h-8 w-8"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                />
+              </svg>
+            </div>
+          </a>
+          <span className="text-2xl">or</span>
+          <a
+            href="mailto:motionmediaagencysrl@gmail.com"
+            className=" flex w-full items-center justify-center gap-4 rounded-lg border border-gray-300 border-opacity-30 bg-white bg-opacity-5 px-8 py-4"
+          >
+            <span className="text-3xl font-black ">Email</span>
+            <div className="rounded-full ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="h-8 w-8"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                />
+              </svg>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
--- components/sections/Landing.tsx
+++ components/sections/Landing.tsx
@@ -6,18 +6,21 @@ function Landing() {
     <section className="flex h-screen w-full flex-col items-center justify-center">
       <div className="h-20 w-full"></div>
       <div className="flex w-full flex-1 flex-col items-center justify-center p-5 lg:p-20 xl:flex-row">
-        <div className="flex h-full w-full select-none flex-col justify-start gap-10 rounded-xl border border-gray-200 border-opacity-30 p-5 backdrop-blur-sm  md:items-start xl:w-3/5 xl:p-10">
-          <span className="text-[26px] font-black uppercase leading-tight sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl xl:leading-none">
-            Harness the Energy of Motion for Impactful outcomes.
-          </span>
-          <span className="text-xl sm:text-2xl">
+        <div className="flex h-full w-full select-none flex-col items-center gap-10 rounded-xl border border-gray-200 border-opacity-30 p-5 text-center backdrop-blur-sm sm:items-start sm:justify-start sm:text-left lg:gap-[60px] xl:w-3/5 xl:p-10">
+          <div className="flex flex-col text-4xl font-black uppercase leading-tight sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl xl:leading-none">
+            <span>Harness the Energy of</span>
+            <span>Motion for Impactful</span>
+            <span>outcomes.</span>
+          </div>
+          <span className="text-xl opacity-50 sm:text-2xl  lg:text-4xl">
             Remove Barriers, Embrace Velocity
           </span>
-          <div className=" flex pl-4">
+          <div className=" flex sm:pl-4">
             <Link
               href={"https://calendly.com/motionmediaagency/30min"}
               target="_blank"
-              className="animate-testAnimation1 select-none rounded-lg border border-gray-300 border-opacity-30 bg-gray-300 bg-opacity-[3%] px-6 py-3 text-2xl  transition-all hover:bg-opacity-10"
+              style={{ textShadow: "0px 0px 5px white, 0px 0px 8px #5da5f9 " }}
+              className="hover: animate-testAnimation1 select-none rounded-lg bg-white bg-opacity-90 px-6 py-3  text-2xl font-black text-blue-400 mix-blend-screen transition-all duration-200 hover:bg-opacity-80"
             >
               Book Now!
             </Link>
